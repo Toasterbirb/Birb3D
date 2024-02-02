93e1e53e144b5ed716132851aee9e8f6c8928f82
Toasterbirb <git@toasterbirb.com>
2024-02-02 16:22:14 +0200

    Print errors and fatal errors to cerr

--- engine/core/include/Logger.hpp
+++ engine/core/include/Logger.hpp
@@ -20,13 +20,13 @@ namespace birb
 	// Print a red error message
 	static void log_error(const std::string& text)
 	{
-		std::cout << "\033[31m[E] " << text << "\033[0m\n";
+		std::cerr << "\033[31m[E] " << text << "\033[0m\n";
 	}
 
 	// Print a fatal error and crash
 	static void log_fatal(const std::string& text, const int exit_code = 1)
 	{
-		std::cout << "\033[31mFATAL ERROR: " << text << "\033[0m\n";
+		std::cerr << "\033[31mFATAL ERROR: " << text << "\033[0m\n";
 		exit(exit_code);
 	}
 }
