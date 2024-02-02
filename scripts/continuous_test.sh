de4cbe3d1e333c500f7d772f9606dc73031505f4
Toasterbirb <git@toasterbirb.com>
2024-02-02 02:04:18 +0200

    Create the build directory if it doesn't exist already

--- scripts/continuous_test.sh
+++ scripts/continuous_test.sh
@@ -7,6 +7,8 @@
 PROJECT_ROOT="$(dirname "$0")/.."
 BUILD_DIR="$PROJECT_ROOT/build"
 
+mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR" || exit 1
+cmake ..
 
 find "$PROJECT_ROOT/engine" "$PROJECT_ROOT/tests" -iname "*.[hc]pp" | entr -sc 'make test'
