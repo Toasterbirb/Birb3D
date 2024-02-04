bbaa63555f3d635c5d770b2e906c6527d77b9deb
Toasterbirb <git@toasterbirb.com>
2024-02-04 17:51:44 +0200

    Add a color struct

---
+++ engine/core/include/Color.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "Vector.hpp"
+
+namespace birb
+{
+	struct color
+	{
+		float r, g, b, a;
+
+		constexpr color() : r(0), g(0), b(0), a(0) {}
+		constexpr color(float r, float g, float b, float a = 1.0f) : r(r), g(g), b(b), a(a) {}
+		constexpr color(double r, double g, double b, double a = 1.0) : r(r), g(g), b(b), a(a) {}
+
+		constexpr color(int r, int g, int b, int a = 255)
+		{
+			constexpr float reverse_division = 1 / 255.0f;
+			this->r = r * reverse_division;
+			this->g = g * reverse_division;
+			this->b = b * reverse_division;
+			this->a = a * reverse_division;
+		}
+
+		// Construct a color from a hex value
+		constexpr color(int hex)
+		:a(1.0f)
+		{
+			constexpr float reverse_division = 1 / 255.0f;
+			this->r = ((hex & 0xff0000) >> 16) * reverse_division;
+			this->g = ((hex & 0xff00) >> 8) * reverse_division;
+			this->b = ((hex & 0xff) >> 0) * reverse_division;
+		}
+
+		constexpr bool operator==(const color& other) const
+		{
+			return 	(this->r == other.r
+					&& this->g == other.g
+					&& this->b == other.b
+					&& this->a == other.a);
+		}
+
+		constexpr bool operator!=(const color& other) const
+		{
+			return 	(this->r != other.r
+					|| this->g != other.g
+					|| this->b != other.b
+					|| this->a != other.a);
+		}
+	};
+}
---
+++ tests/color.cpp
@@ -0,0 +1,65 @@
+#include "Color.hpp"
+#include "Math.hpp"
+
+#include <doctest/doctest.h>
+
+TEST_CASE("Color constructor")
+{
+	SUBCASE("Default")
+	{
+		birb::color color;
+		CHECK(color.r == 0);
+		CHECK(color.g == 0);
+		CHECK(color.b == 0);
+		CHECK(color.a == 0);
+	}
+
+	SUBCASE("Float")
+	{
+		birb::color color(0.1f, 0.2f, 0.3f, 0.4f);
+		CHECK(color.r == 0.1f);
+		CHECK(color.g == 0.2f);
+		CHECK(color.b == 0.3f);
+		CHECK(color.a == 0.4f);
+	}
+
+	SUBCASE("Double")
+	{
+		birb::color color(0.1, 0.2, 0.3, 0.4);
+		CHECK(color.r == 0.1f);
+		CHECK(color.g == 0.2f);
+		CHECK(color.b == 0.3f);
+		CHECK(color.a == 0.4f);
+	}
+
+	SUBCASE("Int")
+	{
+		birb::color color(51, 51, 51);
+		CHECK(birb::round(color.r, 2) == birb::round(0.2f, 2));
+		CHECK(birb::round(color.g, 2) == birb::round(0.2f, 2));
+		CHECK(birb::round(color.b, 2) == birb::round(0.2f, 2));
+		CHECK(birb::round(color.a, 2) == birb::round(1.0f, 2));
+	}
+
+	SUBCASE("Hex")
+	{
+		birb::color color(0x333333);
+		CHECK(birb::round(color.r, 2) == birb::round(0.2f, 2));
+		CHECK(birb::round(color.g, 2) == birb::round(0.2f, 2));
+		CHECK(birb::round(color.b, 2) == birb::round(0.2f, 2));
+		CHECK(birb::round(color.a, 2) == birb::round(1.0f, 2));
+	}
+}
+
+TEST_CASE("Color comparison operator")
+{
+	birb::color color_a(0, 1, 1, 1);
+	birb::color color_b(1, 1, 1, 0);
+	birb::color color_c(1, 1, 0, 1);
+	birb::color color_d(1, 0, 1, 1);
+
+	CHECK(color_a == color_a);
+	CHECK_FALSE(color_a == color_b);
+	CHECK_FALSE(color_a == color_c);
+	CHECK_FALSE(color_a == color_d);
+}
