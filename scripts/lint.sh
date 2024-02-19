f5c3589d7409dfbdefb4ee2d7a9d1c671af04777
Toasterbirb <git@toasterbirb.com>
2024-02-20 01:57:58 +0200

    Combine shellcheck and cppcheck into a singular linting script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,4 +2,10 @@
 
 PROJECT_ROOT="$(dirname "$0")/.."
 
+SHELL_SCRIPTS="$(find "$PROJECT_ROOT/scripts" "$PROJECT_ROOT/engine" -iname "*.sh")"
+for SCRIPT in $SHELL_SCRIPTS
+do
+	shellcheck -s sh -S style "$SCRIPT"
+done
+
 cppcheck --include="$PROJECT_ROOT/engine/core/include/Profiling.hpp" -q --enable=warning,style,performance,portability --check-level=exhaustive "$PROJECT_ROOT/engine" "$PROJECT_ROOT/tests" "$PROJECT_ROOT/demos"
