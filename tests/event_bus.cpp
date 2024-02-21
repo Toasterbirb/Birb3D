b29806e998faed6eb208749b135299739a07e2e2
Toasterbirb <git@toasterbirb.com>
2024-02-21 09:53:44 +0200

    Fix a bug in the event bus unit test

--- tests/event_bus.cpp
+++ tests/event_bus.cpp
@@ -12,20 +12,20 @@ struct test_obj : public birb::event_obj
 	void process_event(unsigned short event_id, const birb::event_data& data)
 	{
 		if (event_id == 999)
-			success = data._int[2];
+			result = data._int[2];
 	}
 
-	bool success = false;
+	int result = 0;
 };
 
 TEST_CASE("Event bus")
 {
 	test_obj obj;
-	CHECK(obj.success == false);
+	CHECK(obj.result == 0);
 
 	birb::event_data data;
-	data._bool[2] = 42;
+	data._int[2] = 42;
 	birb::event_bus::send_event(999, data);
 
-	CHECK(obj.success == true);
+	CHECK(obj.result == 42);
 }
