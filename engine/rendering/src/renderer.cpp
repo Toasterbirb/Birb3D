6fcaa1144ac410a77b0cd251ebb1806010529855
Toasterbirb <git@toasterbirb.com>
2024-02-21 08:12:57 +0200

    Clean out old commented out code from the renderer

--- engine/rendering/src/renderer.cpp
+++ engine/rendering/src/renderer.cpp
@@ -29,31 +29,10 @@ namespace birb
 	renderer::~renderer()
 	{
 		birb::log("Crushing the renderer");
-		// glDeleteProgram(shader_program);
 	}
 
 	void renderer::draw_verts(std::vector<float> verts)
 	{
-		// glGenVertexArrays(1, &VAO);
-		// glGenBuffers(1, &VBO);
 
-		// glBindVertexArray(VAO);
-
-		// glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		// glBufferData(GL_ARRAY_BUFFER, 3, verts.data(), GL_STATIC_DRAW);
-
-		// glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		// glEnableVertexAttribArray(0);
-
-		// glBindBuffer(GL_ARRAY_BUFFER, 0);
-		// glBindVertexArray(0);
-
-		// These funcs need to be moved out of here at some point
-		// glUseProgram(shader_program);
-		// glBindVertexArray(VAO);
-		// glDrawArrays(GL_TRIANGLES, 0, 3);
-
-
-		// TODO: Delete the vertex array and buffers at some point Ow
 	}
 }
