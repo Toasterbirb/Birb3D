79139c36364ecf78c8139d6b96c5c1204811dcd9
Toasterbirb <git@toasterbirb.com>
2024-02-22 19:22:07 +0200

    Make the hex to float conversion program output floating point values in vector mode

--- engine/utils/hex_to_float.cpp
+++ engine/utils/hex_to_float.cpp
@@ -1,18 +1,40 @@
 #include "Color.hpp"
 
+#include <string.h>
 #include <iostream>
+
+enum mode {
+	vec, def
+};
 
 int main(int argc, char** argv)
 {
-	if (argc != 2)
+	if (argc < 2)
 	{
 		std::cout << "Usage: ./hex_to_float FF00AA\n";
 		return 1;
 	}
 
+	/* Different modes */
+	mode mode = mode::def;
+	if (argc == 3)
+	{
+		if (!strcmp(argv[2], "vec"))
+			mode = mode::vec;
+	}
+
 	birb::color color(std::stoi(argv[1], 0, 16));
 
-	std::cout << color.r << " " << color.g << " " << color.b << "\n";
+	switch (mode)
+	{
+		case def:
+			std::cout << color.r << " " << color.g << " " << color.b << "\n";
+			break;
+
+		case vec:
+			std::cout << color.r << "f, " << color.g << "f, " << color.b << "f, 1.0f\n";
+			break;
+	}
 
 	return 0;
 }
