1245af37151679a0b7e5c55ed0186f660639dfde
Toasterbirb <git.53jvg@aleeas.com>
2024-03-03 01:38:05 +0200

    Fix indentation

--- engine/widgets/include/WidgetBase.hpp
+++ engine/widgets/include/WidgetBase.hpp
@@ -4,17 +4,17 @@
 
 namespace birb
 {
-		struct widget_base
-		{
-		public:
-			virtual void draw() = 0;
+	struct widget_base
+	{
+	public:
+		virtual void draw() = 0;
 
-			float padding = 5.0f;
-			float bg_alpha = 0.45f;
+		float padding = 5.0f;
+		float bg_alpha = 0.45f;
 
-		protected:
-			void setup_overlay();
+	protected:
+		void setup_overlay();
 
-			ImGuiWindowFlags overlay_window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoMove;
-		};
+		ImGuiWindowFlags overlay_window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoMove;
+	};
 }
