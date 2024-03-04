9ebd3c48e6d98f99eb6867237efade9fb7899bd7
Toasterbirb <git@toasterbirb.com>
2024-03-05 01:40:15 +0200

    Make the directional light dragfloat slower and easier to control

--- editor/src/world.cpp
+++ editor/src/world.cpp
@@ -11,7 +11,7 @@ namespace editor
 		{
 			if (ImGui::CollapsingHeader("Directional light"))
 			{
-				ImGui::DragFloat3("Direction", *birb::shader::directional_direction.to_ptr_array().data());
+				ImGui::DragFloat3("Direction", *birb::shader::directional_direction.to_ptr_array().data(), 0.02f);
 				ImGui::Spacing();
 				ImGui::ColorEdit3("Ambient", *birb::shader::directional_ambient.to_ptr_array().data());
 				ImGui::ColorEdit3("Diffuse", *birb::shader::directional_diffuse.to_ptr_array().data());
