e3a5f45ebc063564dc15f6957ce0b9bc36a4a127
Toasterbirb <git@toasterbirb.com>
2024-03-05 15:13:16 +0200

    Process the editor directory when linting

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,12 +2,12 @@
 
 PROJECT_ROOT="$(dirname "$0")/.."
 
-SHELL_SCRIPTS="$(find "$PROJECT_ROOT/scripts" "$PROJECT_ROOT/engine" -iname "*.sh")"
+SHELL_SCRIPTS="$(find "$PROJECT_ROOT/scripts" "$PROJECT_ROOT/engine" "$PROJECT_ROOT/editor" -iname "*.sh")"
 for SCRIPT in $SHELL_SCRIPTS
 do
 	shellcheck -s sh -S style "$SCRIPT"
 done
 
-cppcheck --include="$PROJECT_ROOT/engine/core/include/Profiling.hpp" -q --enable=warning,style,performance,portability --check-level=exhaustive "$PROJECT_ROOT/engine" "$PROJECT_ROOT/tests" "$PROJECT_ROOT/demos"
+cppcheck --include="$PROJECT_ROOT/engine/core/include/Profiling.hpp" -q --enable=warning,style,performance,portability --check-level=exhaustive "$PROJECT_ROOT/engine" "$PROJECT_ROOT/tests" "$PROJECT_ROOT/demos" "$PROJECT_ROOT/editor"
 
 "$PROJECT_ROOT/scripts/event_id_linter.sh"
--- scripts/list_event_ids.sh
+++ scripts/list_event_ids.sh
@@ -7,7 +7,7 @@
 PROJECT_ROOT="$(dirname "$0")/.."
 
 # Scan the engine directory
-EVENT_LIST="$(grep -r "event_bus::register_event_id([0-9]*, this); //" "$PROJECT_ROOT/engine" "$PROJECT_ROOT/tests")"
+EVENT_LIST="$(grep -r "event_bus::register_event_id([0-9]*, this); //" "$PROJECT_ROOT/engine" "$PROJECT_ROOT/editor" "$PROJECT_ROOT/tests")"
 
 IFS=$(printf "\n\b")
 for i in $EVENT_LIST
