ef0f3aa5e03088f29e12617aa1de9557efd31266
Toasterbirb <git@toasterbirb.com>
2024-05-18 18:25:40 +0300

    Use using instead of typedef for type aliases

--- engine/core/include/Types.hpp
+++ engine/core/include/Types.hpp
@@ -1,20 +1,20 @@
 #include <cstdint>
 
-typedef int8_t	i8;
-typedef int16_t	i16;
-typedef int32_t	i32;
-typedef int64_t	i64;
+using i8	= int8_t;
+using i16	= int16_t;
+using i32	= int32_t;
+using i64	= int64_t;
 
-typedef uint8_t		u8;
-typedef uint16_t	u16;
-typedef uint32_t	u32;
-typedef uint64_t	u64;
+using u8	= uint8_t;
+using u16	= uint16_t;
+using u32	= uint32_t;
+using u64	= uint64_t;
 
-typedef float f32;
-typedef double f64;
-typedef long double f128;
+using f32	= float;
+using f64	= double;
+using f128	= long double;
 
-typedef float floatymcfloatface;
+using floatymcfloatface = float;
 
 static_assert(sizeof(f32) == sizeof(i32));
 static_assert(sizeof(f64) == sizeof(i64));
