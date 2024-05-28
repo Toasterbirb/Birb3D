5126e95b71833d6c510a9d3713308d5e6b22d81d
Toasterbirb <git@toasterbirb.com>
2024-05-28 14:59:24 +0300

    Avoid repetitive assertions with a macro in shader uniform set functions

--- engine/rendering/src/shader_set_funcs.cpp
+++ engine/rendering/src/shader_set_funcs.cpp
@@ -7,15 +7,22 @@
 #include <glm/ext.hpp>
 #include <glm/glm.hpp>
 
+#ifndef NDEBUG
+
+#define UNIFORM_SET_ASSERTS(TYPE) \
+	birb::ensure(!uniform.name.empty(), "The uniform name is empty"); \
+	birb::ensure(uniform.type == TYPE, "Uniform type mismatch"); \
+	birb::ensure(d_currently_active_shader == id, "The shader needs to be activated before it can be modified");
+
+#else
+#define UNIFORM_SET_ASSERTS(TYPE)
+#endif
+
 namespace birb
 {
-	static constexpr char SHADER_NOT_ACTIVATED_ASSERT_ERROR[] = "The shader needs to be activated before it can be modified";
-
 	void shader::set(const uniform& uniform, i32 value, i32 index)
 	{
-		ensure(!uniform.name.empty());
-		ensure(uniform.type == uniform_type::INT);
-		ensure(d_currently_active_shader == id, SHADER_NOT_ACTIVATED_ASSERT_ERROR);
+		UNIFORM_SET_ASSERTS(uniform_type::INT);
 
 		const i32 key = uniform_location(uniform.str(index));
 		if (uniform_cache(key, value, uniform_cache_int))
@@ -27,9 +34,7 @@ namespace birb
 
 	void shader::set(const uniform& uniform, f32 value, i32 index)
 	{
-		ensure(!uniform.name.empty());
-		ensure(uniform.type == uniform_type::FLOAT);
-		ensure(d_currently_active_shader == id, SHADER_NOT_ACTIVATED_ASSERT_ERROR);
+		UNIFORM_SET_ASSERTS(uniform_type::FLOAT);
 
 		const i32 key = uniform_location(uniform.str(index));
 		if (uniform_cache(key, value, uniform_cache_float))
@@ -41,9 +46,7 @@ namespace birb
 
 	void shader::set(const uniform& uniform, const glm::vec2 value, i32 index)
 	{
-		ensure(!uniform.name.empty());
-		ensure(uniform.type == uniform_type::VEC2);
-		ensure(d_currently_active_shader == id, SHADER_NOT_ACTIVATED_ASSERT_ERROR);
+		UNIFORM_SET_ASSERTS(uniform_type::VEC2);
 
 		const i32 key = uniform_location(uniform.str(index));
 		if (uniform_cache(key, value, uniform_cache_vec2))
@@ -55,9 +58,7 @@ namespace birb
 
 	void shader::set(const uniform& uniform, const glm::vec3 value, i32 index)
 	{
-		ensure(!uniform.name.empty());
-		ensure(uniform.type == uniform_type::VEC3);
-		ensure(d_currently_active_shader == id, SHADER_NOT_ACTIVATED_ASSERT_ERROR);
+		UNIFORM_SET_ASSERTS(uniform_type::VEC3);
 
 		const i32 key = uniform_location(uniform.str(index));
 		if (uniform_cache(key, value, uniform_cache_vec3))
@@ -69,9 +70,7 @@ namespace birb
 
 	void shader::set(const uniform& uniform, const glm::vec4 value, i32 index)
 	{
-		ensure(!uniform.name.empty());
-		ensure(uniform.type == uniform_type::VEC4);
-		ensure(d_currently_active_shader == id, SHADER_NOT_ACTIVATED_ASSERT_ERROR);
+		UNIFORM_SET_ASSERTS(uniform_type::VEC4);
 
 		const i32 key = uniform_location(uniform.str(index));
 		if (uniform_cache(key, value, uniform_cache_vec4))
@@ -83,9 +82,7 @@ namespace birb
 
 	void shader::set(const uniform& uniform, const birb::vec3<f32> value, i32 index)
 	{
-		ensure(!uniform.name.empty());
-		ensure(uniform.type == uniform_type::BIRB_VEC3_FLOAT);
-		ensure(d_currently_active_shader == id, SHADER_NOT_ACTIVATED_ASSERT_ERROR);
+		UNIFORM_SET_ASSERTS(uniform_type::BIRB_VEC3_FLOAT);
 
 		const i32 key = uniform_location(uniform.str(index));
 		if (uniform_cache(key, value, uniform_cache_birb_vec3_float))
@@ -97,9 +94,7 @@ namespace birb
 
 	void shader::set(const uniform& uniform, const glm::mat4 value, i32 index)
 	{
-		ensure(!uniform.name.empty());
-		ensure(uniform.type == uniform_type::MAT4);
-		ensure(d_currently_active_shader == id, SHADER_NOT_ACTIVATED_ASSERT_ERROR);
+		UNIFORM_SET_ASSERTS(uniform_type::MAT4);
 
 		const i32 key = uniform_location(uniform.str(index));
 		if (uniform_cache(key, value, uniform_cache_mat4))
@@ -111,9 +106,7 @@ namespace birb
 
 	void shader::set(const uniform& uniform, const color value, i32 index)
 	{
-		ensure(!uniform.name.empty());
-		ensure(uniform.type == uniform_type::BIRB_COLOR);
-		ensure(d_currently_active_shader == id, SHADER_NOT_ACTIVATED_ASSERT_ERROR);
+		UNIFORM_SET_ASSERTS(uniform_type::BIRB_COLOR);
 
 		activate();
 		glUniform3f(uniform_location(uniform.str(index)), value.r, value.g, value.b);
