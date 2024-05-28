dc5bcce9a0850dfb3fb496d5cdad8f51b3785aa6
Toasterbirb <git@toasterbirb.com>
2024-05-28 15:41:33 +0300

    Use a transparent background clear color
    Fixes #11 (hopefully)

--- engine/core/src/window.cpp
+++ engine/core/src/window.cpp
@@ -92,7 +92,9 @@ namespace birb
 		window::window_size_changed = false;
 
 		// Set the default background color to something other than black
-		set_background_color(0x1F1F28);
+		color background_color = 0x1F1F28;
+		background_color.a = 0.0f;
+		set_background_color(background_color);
 
 		// Enable depth testing
 		glEnable(GL_DEPTH_TEST);
