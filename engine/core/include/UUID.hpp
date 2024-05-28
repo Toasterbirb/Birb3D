1b87bbffa25c2d0e3b979bd8f083753e61e085c6
Toasterbirb <git@toasterbirb.com>
2024-05-28 15:23:59 +0300

    Add a class for generating unique identifiers

---
+++ engine/core/include/UUID.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Random.hpp"
+
+namespace birb
+{
+	class uuid
+	{
+	public:
+		uuid() = delete;
+		~uuid() = delete;
+		uuid(const uuid&) = delete;
+		uuid(uuid&) = delete;
+
+		/**
+		 * @brief Returns a random 64bit unsigned integer
+		 *
+		 * The uniqueness of this identifier is not guaranteed, but the
+		 * chance of collision *should* be vanishingly small
+		 */
+		static u64 generate();
+
+	private:
+		static inline random rng;
+	};
+}
