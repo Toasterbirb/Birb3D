85528348a73e92c702d23f7d27c30bb280503537
Toasterbirb <git@toasterbirb.com>
2024-05-29 20:19:36 +0300

    Add an option for customizing the job count

--- scripts/continuous_test_ninja.sh
+++ scripts/continuous_test_ninja.sh
@@ -4,6 +4,9 @@
 # Build and run tests on file change #
 ######################################
 
+JOB_COUNT="$1"
+[ -z "$JOB_COUNT" ] && echo "Defaulting to $(nproc) threads" && JOB_COUNT="$(nproc)"
+
 PROJECT_ROOT="$(dirname "$0")/.."
 BUILD_DIR="$PROJECT_ROOT/build"
 
@@ -11,4 +14,4 @@ mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR" || exit 1
 cmake .. -G Ninja
 
-find "$PROJECT_ROOT/engine" "$PROJECT_ROOT/demos" "$PROJECT_ROOT/tests" | entr -sc "ninja test"
+find "$PROJECT_ROOT/engine" "$PROJECT_ROOT/demos" "$PROJECT_ROOT/tests" | entr -sc "ninja -j $JOB_COUNT test"
