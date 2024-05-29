57d0e0a2e03504a3c3429bb825c21177628fb335
Toasterbirb <git@toasterbirb.com>
2024-05-29 04:46:46 +0300

    Use nearest filtering for glyph rendering

--- engine/core/src/font_manager.cpp
+++ engine/core/src/font_manager.cpp
@@ -77,8 +77,8 @@ namespace birb
 			// Texture options
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 			character character = {
 				glyph_texture_ids[i],
