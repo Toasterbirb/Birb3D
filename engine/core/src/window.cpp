f36d28cc5e4dde2c0affb70692fd60bc4d361b2b
Toasterbirb <git@toasterbirb.com>
2024-06-05 23:13:21 +0300

    Add an assert that checks if there's an FBO bound when flipping buffers
    Having a custom FBO bound while attempting to draw Dear ImGui stuff
    doesn't seem to end well. It would be a bad idea anyway, so might aswell
    check for it

--- engine/core/src/window.cpp
+++ engine/core/src/window.cpp
@@ -380,6 +380,14 @@ namespace birb
 	{
 		PROFILER_SCOPE_RENDER_FN();
 
+#ifndef NDEBUG
+		// Dear ImGui will cause the application to crash if
+		// there's some custom framebuffer bound when it renders
+		i32 previously_bound_fbo_draw = -1;
+		glGetIntegerv(GL_FRAMEBUFFER_BINDING, &previously_bound_fbo_draw);
+		ensure(previously_bound_fbo_draw == 0, "Remember to unbind all FBOs before flipping the buffers");
+#endif
+
 		{
 			PROFILER_SCOPE_RENDER("ImGui_ImplOpenGL3_NewFrame");
 			ImGui_ImplOpenGL3_NewFrame();
