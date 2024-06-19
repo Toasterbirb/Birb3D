46fe163f7001b44fa43006e0b417d0c9ebf267f2
Toasterbirb <git@toasterbirb.com>
2024-06-19 08:28:27 +0300

    Don't show a stacktrace in the unit test output

--- tests/logger.cpp
+++ tests/logger.cpp
@@ -6,6 +6,6 @@ TEST_CASE("Logging")
 	birb::log("Logging test");
 	birb::log("Some number: ", 10, ", hello ", "world");
 	birb::log_warn("Warning test: ", 0);
-	birb::log_error("Error test: ", 42);
+	birb::log_error_no_trace("Error test: ", 42);
 	FIXME("Fixme test");
 }
